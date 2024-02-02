b57acd04e9caa02c84b8f5cc43c96c66e6176077
aadiisawant <adisawant0178@gmail.com>
2024-02-02 12:28:52 +0530

    Array operations

--- ArrayCheck.js
+++ ArrayCheck.js
@@ -1,4 +1,26 @@
 function checkArray(ele){
     return Array.isArray(ele);
 }
-console.log(checkArray({})); //[]-> array ,{}-> object
+// console.log(checkArray({})); //[]-> array ,{}-> object
+
+
+var arr = [1,2,3,4,5,6]
+//empty the arr without using loops
+// arr.length=0
+// let arr1 =arr.concat(arr)
+// console.log(arr1);
+
+
+
+
+// a is number or not
+let aNum = 15
+// console.log(Number.isInteger(aNum));
+function isNum(aNum){
+    if(aNum %1 ==0){
+        console.log("Its a Number");
+    }else{
+        console.log("Not !");
+    }
+}
+// isNum(aNum)
