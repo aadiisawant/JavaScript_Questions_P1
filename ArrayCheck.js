34164d9dfd77346977fc49a8090ac6aaebec8cb3
aadiisawant <adisawant0178@gmail.com>
2024-02-23 21:57:10 +0530

    Array Ques

--- ArrayCheck.js
+++ ArrayCheck.js
@@ -10,9 +10,6 @@ var arr = [1,2,3,4,5,6]
 // let arr1 =arr.concat(arr)
 // console.log(arr1);
 
-
-
-
 // a is number or not
 let aNum = 15
 // console.log(Number.isInteger(aNum));
@@ -24,3 +21,10 @@ function isNum(aNum){
     }
 }
 // isNum(aNum)
+
+
+// Write a function that takes an array of integers as input and returns a new array with only the unique elements. 
+function uniqueArr(arr){
+    return Array.from(new Set(arr))
+}
+console.log(uniqueArr([1,2,3,1,4,2,5,3,5]));
