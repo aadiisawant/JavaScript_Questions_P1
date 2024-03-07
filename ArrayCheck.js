389cd5a5fc8b39a50333fcea45826d4551216d42
aadiisawant <adisawant0178@gmail.com>
2024-03-07 23:02:24 +0530

    minnor changes

--- ArrayCheck.js
+++ ArrayCheck.js
@@ -91,4 +91,16 @@ const calculate = (a,b)=>{
 // const [add,,,div] = calculate(5,10);
 // console.log(add,div);
 
+// every() – returns true if function returns true on every item
+// filter() – returns an array of all items for which the function returns true
+// forEach() – no return value (just run the function on every element in the list)
+// map() – returns a new list with the result of each item in an array
+// some() – returns true if the function returns true for at least one of the items
 
+var numAr = [1,2,3,4,5,6,99,4,7,8]
+var Every_Num_less = numAr.every(num => num<100)
+// console.log(Every_Num_less);
+var Every_Num_less = numAr.some(num => num<90)
+// console.log(Every_Num_less);
+var filtAr = numAr.filter(num => num%2==0)
+console.log(filtAr);
