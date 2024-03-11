8a701e5e8462ff69bf521da309d7b55e154cb3d6
aadiisawant <adisawant0178@gmail.com>
2024-03-11 22:53:26 +0530

    minor changes

--- Promises.js
+++ Promises.js
@@ -103,4 +103,24 @@ async function consumePromiseThree(){
     console.log(error);  
     }
 }
-consumePromiseThree()
+// consumePromiseThree()
+
+async function getAllUsers(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log('Error: ',error);
+    }
+}
+// getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((data)=>{
+    return data.json()
+}).then((data)=>{
+    console.log(data);
+}).catch(error =>
+    console.log(error)
+)
