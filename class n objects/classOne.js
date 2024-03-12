f8b37cacb07780d902c32e65e7f8c7af867eb011
aadiisawant <adisawant0178@gmail.com>
2024-03-12 17:27:40 +0530

    minor changes

---
+++ class n objects/classOne.js
@@ -0,0 +1,20 @@
+const Person = {
+    username : "adityasawant",
+    name: "Aditya",
+    getFunction : function(){
+        console.log(`username: ${this.username}`)
+        console.log(this);
+    }
+}
+// Person.getFunction()
+
+function user(username,loginCount,isLogedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isLogedIn = isLogedIn
+    this.greet = ()=> console.log(`hello! ${this.username}`)
+    // return this  //implicitly defined
+}
+const userOne = new user("Aditya",12,true)
+const userTwo = new user("Ram",15,false)
+console.log(userTwo.greet());
